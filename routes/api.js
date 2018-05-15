5ca4c12b45fe015892f0572c01c9aeab131bb435
Dylan Spaans <dspaans@avans.nl>
2018-05-15 16:43:37 +0200

    Added CRUD for deelnemers and maaltijd

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,9 @@ var ValidToken = require('../models/ValidToken.js');
 var ApiError = require('../models/ApiError.js');
 var Studentenhuis = require('../models/Studentenhuis.js');
 var StudentenhuisResponse = require('../models/StudentenhuisResponse.js');
-
+var Maaltijd = require('../models/Maaltijd');
+var MaaltijdResponse = require('../models/MaaltijdResponse');
+var DeelnemerResponse = require('../models/DeelnemerResponse');
 
 
 //Catch all except login and register
@@ -156,6 +158,165 @@ router.route('/studentenhuis/:huisId?').delete( function(req, res) {
                     }
                 } else {
                     res.status(404).json(new ApiError("Niet gevonden (huisId bestaat niet)", 404));
+                }
+            });
+        }
+    });
+});
+
+//Maaltijd crud
+
+router.route('/studentenhuis/:huisId?/maaltijd').post( function(req, res) {
+    // Get body params
+
+    var maaltijd = new Maaltijd(req.body.naam, req.body.beschrijving, req.body.ingredienten, req.body.allergie, req.body.prijs)
+
+    if (maaltijd.code){
+        res.status(412).json(maaltijd);
+    }else{
+        var token = (req.header('X-Access-Token')) || '';
+
+        auth.decodeToken(token, (err, payload) => {
+            if (err) {
+                console.log('Error handler: ' + err.message);
+                res.status((err.status || 401 )).json(new ApiError("Niet geautoriseerd (geen valid token)", 401));
+            } else {
+                db.query('INSERT INTO maaltijd SET ?', {Naam: maaltijd.naam, Beschrijving: maaltijd.beschrijving, Ingredienten: maaltijd.ingredienten, Allergie: maaltijd.allergie, Prijs: maaltijd.prijs, UserId: payload.id, StudentenhuisId: db.escape(req.params.huisId)}, function (error, results, fields) {
+                    if (error) throw error;
+                    console.log(results);
+                    res.status(200).json(new MaaltijdResponse(results.insertId, maaltijd.naam, maaltijd.beschrijving, maaltijd.ingredienten, maaltijd.allergie, maaltijd.prijs));
+                });
+            }
+        });
+    }
+});
+
+router.get('/studentenhuis/:huisId?/maaltijd', function(req, res, next) {
+    db.query('SELECT * FROM maaltijd WHERE studentenhuisID = ' + db.escape(req.params.huisId), function (error, results, fields) {
+        if (error) throw error;
+        res.json(results);
+    });
+});
+
+router.get('/studentenhuis/:huisId?/maaltijd/:maaltijdId?', function(req, res, next) {
+    db.query('SELECT * FROM maaltijd  WHERE ID = ' + db.escape(req.params.maaltijdId) + ' AND studentenhuisID = ' + db.escape(req.params.huisId), function (error, results, fields) {
+        if (error) throw error;
+        res.json(results[0]);
+    });
+});
+
+router.route('/studentenhuis/:huisId?/maaltijd/:maaltijdId?').put( function(req, res) {
+
+    // Get body params
+    var maaltijd = new Maaltijd(req.body.naam, req.body.beschrijving, req.body.ingredienten, req.body.allergie, req.body.prijs)
+
+    if (maaltijd.code){
+        res.status(412).json(maaltijd);
+    }else{
+        var token = (req.header('X-Access-Token')) || '';
+
+        auth.decodeToken(token, (err, payload) => {
+            if (err) {
+                console.log('Error handler: ' + err.message);
+                res.status((err.status || 401 )).json(new ApiError("Niet geautoriseerd (geen valid token)", 401));
+            } else {
+                db.query('UPDATE maaltijd SET ? WHERE ID = ' + db.escape(req.params.maaltijdId), {Naam: maaltijd.naam, Beschrijving: maaltijd.beschrijving, Ingredienten: maaltijd.ingredienten, Allergie: maaltijd.allergie, Prijs: maaltijd.prijs}, function (error, results, fields) {
+                    if (error) throw error;
+                    console.log(results);
+                    res.status(200).json(new MaaltijdResponse(req.params.maaltijdId, maaltijd.naam, maaltijd.beschrijving, maaltijd.ingredienten, maaltijd.allergie, maaltijd.prijs));
+                });
+            }
+        });
+    }
+});
+
+router.route('/studentenhuis/:huisId?/maaltijd/:maaltijdId?').delete( function(req, res) {
+
+    // Get body params
+
+    var token = (req.header('X-Access-Token')) || '';
+
+    auth.decodeToken(token, (err, payload) => {
+        if (err) {
+            console.log('Error handler: ' + err.message);
+            res.status((err.status || 401 )).json(new ApiError("Niet geautoriseerd (geen valid token)", 401));
+        } else {
+            db.query('SELECT * FROM maaltijd WHERE ID = ' + db.escape(req.params.maaltijdId), function (error, results, fields) {
+                if (error) throw error;
+                console.log(results);
+                if(results[0]){
+                    if (results[0].UserID == payload.sub){
+                        db.query('DELETE FROM maaltijd WHERE ID = ' + db.escape(req.params.maaltijdId), function (error, results, fields) {
+                            res.status(200).json("Deletion of maaltijd " + req.params.maaltijdId + " was successful");
+                        });
+                    } else {
+                        res.status(409).json(new ApiError("Conflict (Gebruiker mag deze data niet verwijderen)", 409));
+                    }
+                } else {
+                    res.status(404).json(new ApiError("Niet gevonden (huisId of maaltijdId bestaat niet)", 404));
+                }
+            });
+        }
+    });
+});
+
+//Deelnemer crud
+
+router.route('/studentenhuis/:huisId?/maaltijd/:maaltijdId?/deelnemers').post( function(req, res) {
+    // Get body params
+    var studentenhuis = new Studentenhuis(req.body.naam, req.body.adres)
+
+    if (studentenhuis.code){
+        res.status(412).json(studentenhuis);
+    }else{
+        var token = (req.header('X-Access-Token')) || '';
+
+        auth.decodeToken(token, (err, payload) => {
+            if (err) {
+                console.log('Error handler: ' + err.message);
+                res.status((err.status || 401 )).json(new ApiError("Niet geautoriseerd (geen valid token)", 401));
+            } else {
+                db.query('INSERT INTO deelnemers SET ?', {UserID: payload.id, StudentenhuisID: db.escape(req.params.huisId), MaaltijdID: db.escape(req.params.maaltijdId)}, function (error, results, fields) {
+                    if (error) throw error;
+                    console.log(results);
+                    res.status(200).json(new DeelnemerResponse(payload.voornaam, payload.achternaam, payload.email));
+                });
+            }
+        });
+    }
+});
+
+router.get('/studentenhuis/:huisId?/maaltijd/:maaltijdId?/deelnemers', function(req, res, next) {
+    db.query('SELECT * FROM view_deelnemers WHERE StudentenhuisID = ' + db.escape(req.params.huisId) + ' AND maaltijdID = ' + db.escape(req.params.maaltijdId), function (error, results, fields) {
+        if (error) throw error;
+        res.json(results);
+    });
+});
+
+router.route('/studentenhuis/:huisId?/maaltijd/:maaltijdId?/deelnemers').delete( function(req, res) {
+
+    // Get body params
+
+    var token = (req.header('X-Access-Token')) || '';
+
+    auth.decodeToken(token, (err, payload) => {
+        if (err) {
+            console.log('Error handler: ' + err.message);
+            res.status((err.status || 401 )).json(new ApiError("Niet geautoriseerd (geen valid token)", 401));
+        } else {
+            db.query('SELECT * FROM deelnemers WHERE ID = ' + db.escape(req.params.huisId), function (error, results, fields) {
+                if (error) throw error;
+                console.log(results);
+                if(results[0]){
+                    if (results[0].UserID == payload.sub){
+                        db.query('DELETE FROM deelnemers WHERE userID = ' + db.escape(req.payload.id) + ' AND maaltijdID = ' + db.escape(req.params.maaltijdId) + ' AND studentenhuisID = ' + db.escape(req.params.huisId), function (error, results, fields) {
+                            res.status(200).json("Deletion of deelnemer " + req.params.huisId + " was successful");
+                        });
+                    } else {
+                        res.status(409).json(new ApiError("Conflict (Gebruiker mag deze data niet verwijderen)", 409));
+                    }
+                } else {
+                    res.status(404).json(new ApiError("Niet gevonden (huisId of maaltijdId bestaat niet)", 404));
                 }
             });
         }
